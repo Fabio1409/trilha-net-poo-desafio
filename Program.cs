df7adac4c900531f7548c7176985b42c4f72e5e6
Jose Fabio <fabio.7.45@hotmail.com>
2025-10-24 22:04:09 -0300

    Melhorando a visibilidade do codigo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-Iphone iphone = new Iphone("123456789", "iPhone 17", "IMEI123456789", "256GB");
-Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI987654321", "16GB");
-iphone.Ligar();
-nokia.ReceberLigacao();
-iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "987654321", modelo: "Nokia 3310", imei: "IMEI987654321", memoria: "16GB");
+nokia.Ligar();
 nokia.InstalarAplicativo("Snake");
+nokia.InstalarAplicativo("WhatsApp");
 nokia.InstalarAplicativo("Facebook");
+
+System.Console.WriteLine("********************************************************");
+
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "123456789", modelo: "iPhone 17", imei: "IMEI123456789", memoria: "256GB");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
 iphone.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("TikTok");
 
 
