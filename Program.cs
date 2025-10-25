0f1dac7e69134e6bb3750d0674e2cb76aa2d9c87
Jose Fabio <fabio.7.45@hotmail.com>
2025-10-24 21:52:05 -0300

    Implementação do metdo TODO, onde o projeto estava pedindo. além da mudanca da versão do .NET6.0 para a versão .NET9.0 no CSproj

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("123456789", "iPhone 17", "IMEI123456789", "256GB");
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI987654321", "16GB");
+iphone.Ligar();
+nokia.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("Snake");
+nokia.InstalarAplicativo("Facebook");
+iphone.InstalarAplicativo("Instagram");
+
+
